e4978dde7c9e9c433c65685b7424f01f854fb969
YKIM <dyounggkim@gmail.com>
2023-01-22 18:48:23 +0000

    first commit

---
+++ win32_msg_box/win32/win32.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <windows.h>
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
+{
+    MessageBox(NULL, "HELLO", "My first Gui", MB_OK);
+    return 0;
+}
+
+
